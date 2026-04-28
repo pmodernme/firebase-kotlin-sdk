577dc015f5288354860c671c9e69c2a569dffa20
Zoe Van Brunt <zoe@zoelog.io>
2026-04-27 16:43:09 -1000

    Add cancel deferred to get

--- firebase-database/src/jvmMain/kotlin/dev/gitlive/firebase/database/database.kt
+++ firebase-database/src/jvmMain/kotlin/dev/gitlive/firebase/database/database.kt
@@ -204,17 +204,22 @@ public actual open class Query internal actual constructor(
 
     public actual suspend fun get(): DataSnapshot {
         val deferred = CompletableDeferred<DataSnapshot>()
-        android.get().addOnSuccessListener { snapshot ->
-            deferred.complete(DataSnapshot(snapshot, persistenceEnabled))
-        }.addOnFailureListener { exception ->
-            deferred.completeExceptionally(
-                if (exception is com.google.firebase.database.DatabaseException) {
-                    exception.wrap()
-                } else {
-                    exception
-                },
-            )
-        }
+        android.get()
+            .addOnSuccessListener { snapshot ->
+                deferred.complete(DataSnapshot(snapshot, persistenceEnabled))
+            }
+            .addOnFailureListener { exception ->
+                deferred.completeExceptionally(
+                    if (exception is com.google.firebase.database.DatabaseException) {
+                        exception.wrap()
+                    } else {
+                        exception
+                    },
+                )
+            }
+            .addOnCanceledListener {
+                deferred.cancel()
+            }
         return deferred.await()
     }
 
